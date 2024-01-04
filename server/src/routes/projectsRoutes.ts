1bb9c7d948ad1b2f57efad2f5d68988c15d39b21
arthurmousinho <arthursilvamo296@gmail.com>
2024-01-04 13:46:30 -0300

    server: trending enpoint and controller

--- server/src/routes/projectsRoutes.ts
+++ server/src/routes/projectsRoutes.ts
@@ -5,12 +5,15 @@ import { createProject } from "../controllers/projects/createProject";
 import { updateProject } from "../controllers/projects/updateProject";
 import { deleteProject } from "../controllers/projects/deleteProject";
 import { likeProject } from "../controllers/projects/likeProject";
+import { getTrendingProjects } from "../controllers/projects/getTrendingProjects";
 
 export async function projectsRoutes(app: FastifyInstance) {
 
     app.get('/projects', getAllProjects);
 
     app.get('/projects/:id', getProjectById);
+
+    app.get('/projects/trending', getTrendingProjects);
 
     app.post('/projects', createProject);
 
