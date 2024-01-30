72d645b6f4de85c3c27b281833e84ff80967b9f5
arthurmousinho <arthursilvamo296@gmail.com>
2024-01-30 14:10:30 -0300

    server: adding deleteAllUserImages endpoint

--- server/src/routes/storageRoutes.ts
+++ server/src/routes/storageRoutes.ts
@@ -1,11 +1,14 @@
 import { FastifyInstance } from "fastify";
 import { getUploadUrl } from "../controllers/storage/getUploadUrl";
 import { deleteImage } from "../controllers/storage/deleteImage";
+import { deleteAllUserImage } from "../controllers/storage/deleteAllUserImages";
 
 export async function storageRoutes(app: FastifyInstance) {
     
     app.post('/storage/get-upload-url', getUploadUrl);
 
-    app.delete('/storage/delete/:imageUUID', deleteImage)
+    app.delete('/storage/delete/:imageUUID', deleteImage);
+
+    app.delete('/storage/deleteAll/:userId', deleteAllUserImage)
 
 }
